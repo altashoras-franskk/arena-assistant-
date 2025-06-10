a92eb7d95afd2e5faebac4b3cdef56921f1dcc99
altashoras-franskk <luizfelipemurarac@gmail.com>
2025-06-09 22:24:32 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const ARE_NA_CONFIG = {
+  API_BASE: 'https://api.are.na/v2',
+  PROXY: 'https://api.allorigins.win/get?url=',
+  TOKEN: 'qqgab5HYGJdacfSNfiup7_ND1CC1KHWoFgRpcJOGOLo' // Substituir pelo token real
+};
